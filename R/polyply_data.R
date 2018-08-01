9dc36ed4fe2626672007c1137b3f50121d604b27
Russell Hyde <russ_hyde@hotmail.com>
2018-08-01 12:28:50 +0100

    formatting

--- R/polyply_data.R
+++ R/polyply_data.R
@@ -4,13 +4,13 @@
 #'
 #' @format       28 * 3 data.frame; columns are 'common_name', 'body', 'brain'.
 #'
-#' @source       the MASS package dataset `Animals` (but, Dipliodocus -> Diplodocus
-#'   typo was fixed).
+#' @source       the MASS package dataset `Animals` (but, Dipliodocus ->
+#'   Diplodocus typo was fixed).
 #'
 "animals"
 
-#' Mappings from common-name to genus-species or genus-species-subspecies
-#'   for animals in the `animals` dataset
+#' Mappings from common-name to genus-species or genus-species-subspecies for
+#'   animals in the `animals` dataset
 #'
 #' @format       28 * 2 tibble; columns are 'common_name', 'species'.
 #'
@@ -27,7 +27,7 @@
 #'
 #' @format       23 * 3 data.frame; columns 'species', 'family', 'order'
 #'
-#' @source       Used taxize::classification over the NCBI database and the species
-#'    names present in `common_to_species`. Code is given in
-#'    `origins_of_the_datasets.Rmd`.
+#' @source       Used taxize::classification over the NCBI database and the
+#'   species names present in `common_to_species`. Code is given in
+#'   `origins_of_the_datasets.Rmd`.
 "taxonomy"
