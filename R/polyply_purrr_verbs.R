bb11b4a0a4d708e61ebe041eec79d16953a32ffc
Russell Hyde <russ_hyde@hotmail.com>
2018-10-08 11:29:05 +0100

    add docs for `map_pf` function
    use %>% from magrittr

--- R/polyply_purrr_verbs.R
+++ R/polyply_purrr_verbs.R
@@ -4,11 +4,20 @@
 
 ###############################################################################
 
-#' @param        .x
-#' @param        .f
-#' @param        ...
+#' Map a function over a (named) list and return a `poly_frame` object
 #'
+#' See `purrr` for details of `map` functions.
+#'
+#' @param        .x            A list or atomic vector. It should have named
+#'   elements otherwise a poly_frame cannot be constructed.
+#' @param        .f            A function, formula or atomic vector (as in
+#'   input to purrr). The function should return a data-frame when applied to
+#'   an input data-frame.
+#' @param        ...           Further arguments to the function `.f`
+#'
+#' @importFrom   magrittr      %>%
 #' @export
+#'
 map_pf <- function(.x, .f, ...) {
   purrr::map(.x, .f, ...) %>%
     as_poly_frame()
