da49e11d00f876ad9935c6f8121f0a424a7e8ac7
Russ Hyde <russHyde@users.noreply.github.com>
2018-10-08 13:00:26 +0100

    fixes #3, fixes #4: add group_by/ungroup/rename (#6)
    New poly_frame methods for modifying the active data-frame were added by
    extending the dplyr S3 methods group_by, ungroup and rename.
    
    Docs for the three methods.
    
    Added new file for housing all dplyr-verb based tests. Currently contains tests
    for group_by, ungroup and rename.

--- R/polyply_dplyr_verbs.R
+++ R/polyply_dplyr_verbs.R
@@ -11,15 +11,29 @@
 #'
 dplyr::filter
 
+#' @importFrom   dplyr         group_by
+#' @export
+#'
+dplyr::group_by
+
 #' @importFrom   dplyr         mutate
 #' @export
 #'
 dplyr::mutate
 
+#' @importFrom   dplyr         rename
+#' @export
+dplyr::rename
+
 #' @importFrom   dplyr         select
 #' @export
 
 dplyr::select
+
+#' @importFrom   dplyr         ungroup
+#' @export
+
+dplyr::ungroup
 
 ###############################################################################
 
@@ -33,6 +47,17 @@ dplyr::select
 
 filter.poly_frame <- build_method_from_dplyr_verb(dplyr::filter)
 
+#' Group the entries in the active data-frame by a set of columns, within a
+#'   poly_frame
+#'
+#' @param        .data         A poly_frame.
+#' @param        ...           Grouping predicates, as would be used in
+#'   dplyr::group_by, and other args to be passed to dplyr::group_by.
+#'
+#' @export
+
+group_by.poly_frame <- build_method_from_dplyr_verb(dplyr::group_by)
+
 #' Mutate the active data-frame within a poly_frame
 #'
 #' @param        .data         A poly_frame.
@@ -43,6 +68,16 @@ filter.poly_frame <- build_method_from_dplyr_verb(dplyr::filter)
 
 mutate.poly_frame <- build_method_from_dplyr_verb(dplyr::mutate)
 
+#' Rename columns in the active data-frame within a poly_frame
+#'
+#' @param        .data         A poly_frame.
+#' @param        ...           Renaming details, as would be used in
+#'   dplyr::rename.
+#'
+#' @export
+
+rename.poly_frame <- build_method_from_dplyr_verb(dplyr::rename)
+
 #' Select within the active data-frame of a poly_frame
 #'
 #' @param        .data         A poly_frame.
@@ -52,3 +87,16 @@ mutate.poly_frame <- build_method_from_dplyr_verb(dplyr::mutate)
 #' @export
 
 select.poly_frame <- build_method_from_dplyr_verb(dplyr::select)
+
+#' Ungroup the columns of the active data-frame, within a poly_frame
+#'
+#' @param        x             A poly_frame.
+#' @param        ...           See dplyr::ungroup
+#'
+#' @export
+
+ungroup.poly_frame <- function(x, ...) {
+  .active_df <- extract_active_df(x)
+  .df <- dplyr::ungroup(.active_df, ...)
+  update_active_df(x, .df)
+}
