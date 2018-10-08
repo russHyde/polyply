4085d2c8f5bc3bca4bf4941a6b832d5448cd1e07
Russell Hyde <russ_hyde@hotmail.com>
2018-10-08 11:12:08 +0100

    fixes #1: map_pf over a list, return a poly_frame
    New function `map_pf`. Given a list (eg, a poly_frame), this will map a
    function over each entry in the list and convert the resulting list to a
    `poly_frame` object.
    
    Tested the function:
      - class(map_pf(...)) == "poly_frame"
      - map_pf(some_list, f, ...) == as_poly_frame(map(some_list, f, ...))
    
    New file polyply_purrr_verbs.R

---
+++ tests/testthat/test_polyply_purrr_verbs.R
@@ -0,0 +1,26 @@
+###############################################################################
+
+context("tests availability of purrr verbs for poly_frames")
+
+###############################################################################
+
+test_that("map should work for poly_frame and return poly_frames", {
+  df1 <- data.frame(a = 1:3, b = letters[1:3])
+  df2 <- data.frame(b = letters[3:6])
+  l1 <- list(df1 = df1, df2 = df2)
+  pf1 <- as_poly_frame(l1)
+
+  expect_is(
+    object = map_pf(pf1, head, 2),
+    "poly_frame",
+    info = "polyply::map(poly_frame, f, ...) should return a poly_frame"
+  )
+
+  expect_equal(
+    object = map_pf(pf1, head, 2),
+    expected = as_poly_frame(
+      purrr::map(l1, head, 2)
+    ),
+    info = "map_pf(poly_frame, f) should equal poly_frame(map(pf_contents, f))"
+  )
+})
